e480e0a21fdfec4ff3dcddd56a04b66c954c4ea9
Serena <serenazsyl@gmail.com>
2024-11-19 11:40:15 -0600

    all models completed, pretty sure

--- models/hobbi-post-model.js
+++ models/hobbi-post-model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
-const postbodySchemas = import("./postbody-schemas.js");
+const postbodySchemas = import("./postbody-schemas");
 
 /**
  * mongoDB schema for posts made on hobbi. see postbody-schemas for the schemas of what actually can be IN a post
@@ -9,18 +9,21 @@ const hobbiPostSchema = new Schema({
     poster: Schema.Types.ObjectId,
     body: {
         type: [postbodySchemas.blockSchema],
+        required: true,
         validate: [(val) => {return val.length <= 10}, '{PATH} exceeds 10 block limit']
     },
-    timestamp: { type: Date, default: Date.now },
     meta: {
+        required: true,
         stars: Number,
         comments: {
-            amount: Number,
-            array: Schema.Types.ObjectId,
+            type: [Schema.Types.ObjectId]
         }
     }
-})
+}, {timestamps: true});
 
 // Apply the discriminators
 hobbiPostSchema.path('body').discriminator('text', postbodySchemas.textBlockSchema);
 hobbiPostSchema.path('body').discriminator('image', postbodySchemas.imageBlockSchema);
+
+const postModel = mongoose.model('hobbi-post', hobbiPostSchema);
+module.exports = postModel;
